9001ed6deae3c1c7030efd3ab2752b8b78673ebf
lavakumar2025 <technicallavakumar@gmail.com>
2025-11-04 14:45:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 from aiohttp import web
-from main.web_support import web_server
+from MeshRenameBot/__main__.web_support import web_server
 from config import *
 import os
 
